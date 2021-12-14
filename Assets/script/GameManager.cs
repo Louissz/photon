50a568d0b5bdd418f39c46c62b96f7833d940ef1
Louis <95418851+Louissz@users.noreply.github.com>
2021-12-14 21:09:03 +0800

    Spawn Points 修改 OnSceneLoaded 函式

--- Assets/script/GameManager.cs
+++ Assets/script/GameManager.cs
@@ -1,5 +1,7 @@
 using Photon.Pun;
 using Photon.Realtime;
+using System.Collections.Generic;
+using Tanks;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,6 +9,7 @@ public class GameManager : MonoBehaviourPunCallbacks
 {
     public static GameManager instance;
     string gameVersion = "1";
+    private GameObject defaultSpawnPoint;
     void Awake()
     {
         if (instance != null)
@@ -19,6 +22,10 @@ public class GameManager : MonoBehaviourPunCallbacks
         PhotonNetwork.AutomaticallySyncScene = true;
         DontDestroyOnLoad(gameObject);
         instance = this;
+
+        defaultSpawnPoint = new GameObject("Default SpawnPoint");
+        defaultSpawnPoint.transform.position = new Vector3(0, 0, 0);
+        defaultSpawnPoint.transform.SetParent(transform, false);
     }
 
     void Start()
@@ -31,11 +38,15 @@ public class GameManager : MonoBehaviourPunCallbacks
     private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
         if (!PhotonNetwork.InRoom)
-
         {
             return;
         }
-        localPlayer = PhotonNetwork.Instantiate("TankPlayer", new Vector3(0, 0, 0), Quaternion.identity, 0);
+        var spawnPoint = GetRandomSpawnPoint();
+        localPlayer = PhotonNetwork.Instantiate(
+          "TankPlayer",
+          spawnPoint.position,
+          spawnPoint.rotation,
+          0);
         Debug.Log("Player Instance ID: " + localPlayer.GetInstanceID());
     }
 
@@ -85,5 +96,25 @@ public class GameManager : MonoBehaviourPunCallbacks
 
     public static GameObject localPlayer;
 
+    public static List<GameObject> GetAllObjectsOfTypeInScene<T>()
+    {
+        var objectsInScene = new List<GameObject>();
+        foreach (var go in (GameObject[])Resources.FindObjectsOfTypeAll(typeof(GameObject)))
+        {
+            if (go.hideFlags == HideFlags.NotEditable ||
+                go.hideFlags == HideFlags.HideAndDontSave)
+                continue;
+            if (go.GetComponent<T>() != null)
+                objectsInScene.Add(go);
+        }
+        return objectsInScene;
+    }
 
+    private Transform GetRandomSpawnPoint()
+    {
+        var spawnPoints = GetAllObjectsOfTypeInScene<SpawnPoint>();
+        return spawnPoints.Count == 0
+        ? defaultSpawnPoint.transform
+        : spawnPoints[Random.Range(0, spawnPoints.Count)].transform;
+    }
 }
